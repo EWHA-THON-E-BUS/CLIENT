40628a5e80943d5bd56d1f69010ccfe708482609
jimin song <songing01@gmail.com>
2024-03-15 21:49:11 +0900

    💄 다크모드 색상 변경

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,11 +4,16 @@ import styled from "styled-components";
 import NoticeBanner from "../components/MainPage/NoticeBanner";
 import BusMap from "../components/MainPage/BusMap";
 import arrow_light from "../assets/arrow_light.svg";
+import arrow_light_grey from "../assets/arrow_light_grey.svg";
 import Board from "../components/MainPage/Board";
 import Footer from "../components/common/Footer";
 import MyStopList from "../components/MainPage/MyStopList";
 import LostItems from "../components/MainPage/LostItems";
+import { useRecoilValue } from "recoil";
+import { themeState } from "../services/store/theme";
 const MainPage = ({ className }) => {
+  const theme = useRecoilValue(themeState);
+
   return (
     <Div className={className}>
       <Header isTheme={true} />
@@ -17,13 +22,24 @@ const MainPage = ({ className }) => {
         <NoticeBanner />
 
         <Container>
-          <div className="maps">
-            <BusMap id={0} />
-            <BusMap id={1} />
-          </div>
+          {theme === "LIGHT" ? (
+            <div className="maps">
+              <BusMap id={0} />
+              <BusMap id={1} />
+            </div>
+          ) : (
+            <div className="maps">
+              <BusMap id={2} />
+            </div>
+          )}
 
           <div className="btn">
-            정류장 전체 위치 보기 <img src={arrow_light} />
+            정류장 전체 위치 보기{" "}
+            {theme === "LIGHT" ? (
+              <img src={arrow_light} />
+            ) : (
+              <img src={arrow_light_grey} />
+            )}
           </div>
         </Container>
 
@@ -78,5 +94,7 @@ const Container = styled.div`
     justify-content: end;
     gap: 4px;
     margin-top: 22px;
+
+    color: var(--theme_black_grey2);
   }
 `;
